37305e9e60581ce50be029bfdf9612066bd41e60
il-fore-24h <foresi.francesco@studenti.salesianisesto.it>
2026-09-25 09:45:16 +0200

    V1.12

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ void main() {
     Scanner sc = new Scanner(System.in);
     System.out.print("Numeri massimi rubrica: ");
     Rubrica rubrica = new Rubrica(sc.nextInt());
-    rubrica.creaContatto(sc.next(),sc.next(),sc.next(),sc.next());
+    while (rubrica.creaContatto(sc.next(),sc.next(),sc.next(),sc.next())){
 
+    }
 
 }
--- src/Rubrica.java
+++ src/Rubrica.java
@@ -3,14 +3,18 @@ public class Rubrica {
     public Rubrica(int lunghezza){
         contattiRubrica = new Contatto[lunghezza];
     }
-    public void creaContatto(String Nome,String Cognome,String Numero,String Etichetta){
-        this.contattiRubrica[1] = new Contatto(Nome, Cognome, Numero, Etichetta);
+    public boolean creaContatto(String Nome,String Cognome,String Numero,String Etichetta){
         for (int i = 0; i <= contattiRubrica.length; i++){
+            if (i >= contattiRubrica.length){
+                return false;
+            }
             if (contattiRubrica[i] == null){
-
+                this.contattiRubrica[i] = new Contatto(Nome, Cognome, Numero, Etichetta);
+                return true;
             }
             System.out.println(contattiRubrica[i]);
         }
+        return false;
     }
 
 }
