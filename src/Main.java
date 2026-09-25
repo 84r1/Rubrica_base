71edb0df4b4a1b6cdd9fd1601a7729e3af3b0c12
il-fore-24h <foresi.francesco@studenti.salesianisesto.it>
2026-09-25 09:53:40 +0200

    V1.13

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,27 @@ import java.util.Scanner;
 
 void main() {
     Scanner sc = new Scanner(System.in);
+    int scelta;
     System.out.print("Numeri massimi rubrica: ");
     Rubrica rubrica = new Rubrica(sc.nextInt());
-    while (rubrica.creaContatto(sc.next(),sc.next(),sc.next(),sc.next())){
+    do {
+        System.out.println("1. Aggiungi contatto \n2. Cerca contatto \n3. Elimina");
+        System.out.print("Scegli: ");
+        scelta = sc.nextInt();
 
-    }
+        switch (scelta){
+            case 1:
+                System.out.print("Inserisci nome: ");
+                String nome = sc.next();
+                System.out.print("Inserisci cognome: ");
+                String cognome = sc.next();
+                System.out.print("Inserisci numero: ");
+                String numero = sc.next();
+                System.out.print("Inserisci etichetta: ");
+                String etichetta = sc.next();
+
+                rubrica.creaContatto(nome, cognome, numero, etichetta);
+        }
+    }while(scelta != 0);
 
 }
